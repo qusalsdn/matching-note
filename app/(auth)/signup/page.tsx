00b63f68934e2a4fbbf2ce1441a60b49e47b5fd0
변민우 <yja05092@gmail.com>
2025-07-16 16:57:29 +0900

    회원가입 흐름 변경

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -18,26 +18,26 @@ export default function Signup() {
     try {
       setLoading(true);
 
-      const {
-        error,
-        data: { user },
-      } = await supabase.auth.signUp(formData);
-
-      if (user?.role !== "authenticated") return toast.error("이미 가입된 이메일입니다.");
-      else if (error) toast.error("오류가 발생했습니다. 다시 시도해주세요.");
-
-      const { data } = await supabase.from("profiles").select("*").eq("nickname", formData.nickname);
+      const { data } = await supabase.from("users").select("*").eq("nickname", formData.nickname);
 
       if (data && data.length > 0) {
         methods.setFocus("nickname");
         return toast.error("닉네임 이미 존재합니다.");
       }
 
-      const { error: insertError } = await supabase.from("profiles").insert({
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signUp(formData);
+
+      if (user?.role !== "authenticated") return toast.error("이미 가입된 이메일입니다.");
+      else if (error) toast.error("오류가 발생했습니다. 다시 시도해주세요.");
+
+      const { error: insertError } = await supabase.from("users").insert({
         user_id: user.id,
         nickname: formData.nickname,
       });
-      if (insertError) return toast.error(insertError.message);
+      if (insertError?.code === "23505") return toast.error("닉네임이 중복됩니다.");
 
       toast.success("회원가입 성공! 이메일 인증 후 로그인해주세요.!");
 
