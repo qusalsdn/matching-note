8cb5aa88a6c8f5543accfaff936205e794092c4d
변민우 <yja05092@gmail.com>
2025-07-17 18:01:46 +0900

    유저 ID를 공통 함수로 분리

---
+++ utils/supabase/getUser.ts
@@ -0,0 +1,16 @@
+import toast from "react-hot-toast";
+import { supabase } from "./client";
+
+export const getUserId = async () => {
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error) return toast.error(error.message);
+
+  const {
+    data: [{ id }],
+  } = (await supabase.from("users").select("id").eq("user_id", user?.id)) as { data: { id: number }[] };
+
+  return id;
+};
